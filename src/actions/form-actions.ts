7a36decf893bc0b9028c7a470072a224160adee1
David <cordoba_d@rocketmail.com>
2024-07-05 18:46:09 -0300

    anda form

--- src/actions/form-actions.ts
+++ src/actions/form-actions.ts
@@ -3,18 +3,34 @@
 import {randomUUID} from 'crypto'
 
 export const createMessage = async (formData: FormData) => {
+    const userName = formData.get("userName") as string | null;
+    const userEmail = formData.get("userEmail") as string | null;
+    const userMessage = formData.get("userMessage") as string | null;
+  
 
-  const userName = formData.get("userName");
-  const userEmail = formData.get("userEmail");
-  const userMessage = formData.get("userMessage");
+    if (!userName || !userEmail || !userMessage) return;
+    
+    if (!userName || !userEmail || !userMessage) {
+        return { error: "Todos los campos son obligatorios." };
+    }
 
-  if (!userName || !userEmail || !userMessage) return;
+    if (userName.length < 2) {
+        return { error: "El nombre debe tener al menos 2 caracteres." };
+    }
 
+    if (userMessage.length < 5) {
+        return { error: "El mensaje debe tener al menos 5 caracteres." };
+    }
 
-  return {
-    id: randomUUID(),
-    name: userName,
-    email: userEmail,
-    message: userMessage
-  }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userEmail)) {
+        return { error: "El correo electronico no es válido." };
+    }
+
+    return {
+        id: randomUUID(),
+        name: userName,
+        email: userEmail,
+        message: userMessage
+    }
 };
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,13 +7,19 @@ import { useRef } from "react";
 const ContactForm = () => {
 
     const formRef = useRef<HTMLFormElement>(null)
+    
   return (
-    <div >
+    <div className={styles.formContainer}>
+    <h2>Contactame</h2>
       <form ref ={formRef} action={async formData => {
         const newMessage = await createMessage(formData)
-        
-        newMessage? alert('Mensaje enviado') : alert('Hay un error en los campos del formulario')
-        formRef.current?.reset()
+        if (!newMessage || 'error' in newMessage) {
+            const errorMessage = newMessage ? newMessage.error : 'Error al enviar el mensaje.';
+            alert(`Error: ${errorMessage}`);
+          } else {
+            alert('Mensaje enviado');
+            formRef.current?.reset();
+          }
       }}>
         <input type="text" placeholder="Nombre" 
           
@@ -24,9 +30,8 @@ const ContactForm = () => {
           name="userEmail"
         />
 
-        <textarea name="userMessage" id="" placeholder="Dejame un mensaje"></textarea>
-        <button
-          >Enviar</button>
+        <textarea name="userMessage" rows={2} id="" placeholder="Dejame un mensaje"></textarea>
+        <button>Enviar</button>
       </form>
     </div>
   )
